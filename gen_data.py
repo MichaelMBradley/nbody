e38e7bc7a1f393fb7ec2194fde68fedbf4d60e6a
Michael Bradley <michaelmbrad@gmail.com>
2023-10-07 17:13:15 -0400

    Create script to generate data

---
+++ gen_data.py
@@ -0,0 +1,34 @@
+#!./venv/bin/python
+import argparse
+from random import uniform, randint
+
+
+class Args:
+    width: int
+    height: int
+    velocity: float
+    mass: float
+    count: int
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        prog="n-body data generator",
+        description="Generates data for the n-body simulator.",
+        add_help=False
+    )
+
+    parser.add_argument("-w", "--width", type=int, default=1900)
+    parser.add_argument("-h", "--height", type=int, default=1000)
+    parser.add_argument("-v", "--velocity", type=float, default=1.)
+    parser.add_argument("-m", "--mass", type=float, default=1.)
+    parser.add_argument("-c", "--count", type=int, default=500)
+
+    args: Args = parser.parse_args()
+
+    for _ in range(args.count):
+        print(f"{randint(-args.width // 2, args.width // 2)},"
+              f"{randint(-args.height // 2, args.height // 2)},"
+              f"{uniform(-args.velocity, args.velocity)},"
+              f"{uniform(-args.velocity, args.velocity)},"
+              f"{uniform(1e-2, args.mass)}")
